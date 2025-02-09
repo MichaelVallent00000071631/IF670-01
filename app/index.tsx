ee6e02d9577cb1a5a3327c7dbee816a533d4191d
MichaelVallent00000071631 <150875514+MichaelVallent00000071631@users.noreply.github.com>
2025-02-09 15:54:34 +0700

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
+
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Michael Vallent - 00000071631</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#000000',
+  },
+});
